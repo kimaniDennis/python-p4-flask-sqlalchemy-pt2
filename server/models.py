2b6fe942714ffd19dedb8dfb3f68b3660a548782
dennis-sketch <dennis.kimani2@student.moringaschool.comgit config --global user.name dennis-sketch>
2024-07-29 15:53:23 +0300

    completed

--- server/models.py
+++ server/models.py
@@ -5,7 +5,7 @@ metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 
-db = SQLAlchemy(metadata=metadata)
+db = SQLAlchemy()
 
 class Owner(db.Model):
     __tablename__ = 'owners'
